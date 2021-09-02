ab72e2d1dc07710c1bb1b32a96a5220474ab5593
adrianoatmenezes <retoretoreto21@gmail.com>
2021-09-01 22:37:52 -0300

    ++

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public hora: any;
+  public minuto: string;
+  public segundo: string;
+  public ampm: string;
+
   constructor() {}
 
+  ngOnInit(){
+    setInterval(() =>{
+      const data = new Date();
+      this.updateDate(data);
+    }, 1000);
+
+  }
+  private updateDate(data: Date){
+      const horas = data.getHours();
+
+      this.ampm = horas >= 12 ? 'PM' : 'AM';
+
+      this.hora = horas % 12;
+      this.hora = this.hora ? this.hora : 12;
+
+      this.hora = this.hora < 10 ? '0' + this.hora : this.hora;
+
+      const minutos = data.getMinutes();
+      this.minuto = minutos < 10 ? '0' + minutos : minutos.toString();
+
+      const segundos = data.getSeconds();
+      this.segundo = segundos < 10 ? '0' + segundos : segundos.toString();
+  }
 }
